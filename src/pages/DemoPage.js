e0835b34a5659f4bae50eb1e1d5fbfca1b9bc011
hongggy <hongggy@gmail.com>
2024-08-22 22:43:31 +0900

    Set prettier, eslint, stylelint, husky

--- src/pages/DemoPage.js
+++ src/pages/DemoPage.js
@@ -1,10 +1,10 @@
-import { Demo } from "../components/Demo/DemoComponent";
+import { Demo } from '../components/Demo/DemoComponent';
 
 export function DemoPage() {
   return (
     <>
       데모페이지 입니다.
-      <Demo />;
+      <Demo />
     </>
   );
 }
