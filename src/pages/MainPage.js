375e2bb491bd3493c59a79af950e6a23bf9f5c07
hongggy <hongggy@gmail.com>
2024-08-23 16:13:26 +0900

    Docs : 스타일린트 설정 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,8 +2,8 @@ import { Demo } from "../components/Demo/DemoComponent";
 
 export function MainPage() {
   return ( <><h1>
-    메인 페이지 입니다. 내용을 입력해주세요 !
-    
+    메인 페이지 입니다. 내용
+
     </h1>
   <Demo /></>);
 }
