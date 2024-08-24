9574e7812464ba3a22530ea536d2011329bbd81d
hongggy <hongggy@gmail.com>
2024-08-24 15:03:03 +0900

    브랜치 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,15 @@
-module.exports = {
-  // 이 ESLint 설정은 이 설정 파일이 있는 디렉토리를 기준으로 모든 하위 디렉토리와 파일에 적용된다.
-  root: true,
 
-  // 실행 환경을 정의. 여기서는 브라우저와 Node.js 환경에서 실행되는 코드를 대상으로 한다.
-  env: {
-    browser: true,
-    node: true,
-  },
-
-  // TypeScript 코드를 분석하기 위한 파서
-  parser: "@typescript-eslint/parser",
-
-  // 파서에 전달되는 옵션들을 정의한다.
-  parserOptions: {
-    // ECMAScript 버전을 2020(ES11)으로 지정
-    ecmaVersion: 2020,
-    // 모듈 시스템을 사용하는 코드를 분석하도록 설정
-    sourceType: "module",
-    // 타입 정보에 기반한 ESLint 규칙을 사용하기 위해 tsconfig.json 파일의 경로를 지정
-    project: "./tsconfig.json",
-    // tsconfig.json 파일의 위치를 지정. __dirname은 현재 파일의 위치를 나타낸다.
-    tsconfigRootDir: __dirname,
-    // JSX 문법을 사용할 수 있도록 설정
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-
-  // ESLint에 추가적인 기능을 제공하는 플러그인들을 명시
-  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
-
-  extends: [
-    "eslint:recommended", // ESLint의 기본 권장 규칙
-    "plugin:react/recommended", // React를 위한 권장 규칙
-    "plugin:react-hooks/recommended", // React Hooks를 위한 권장 규칙
-    "plugin:@typescript-eslint/recommended", // TypeScript를 위한 권장 규칙
-    "plugin:prettier/recommended", // Prettier와 ESLint를 함께 사용하기 위한 설정
-  ],
-
-  // 특정 규칙들을 오버라이드하거나 추가하는 섹션
-  rules: {
-    // React를 사용할 때 'React'를 import 하지 않아도 되게 설정
-    "react/jsx-uses-react": 0,
-    "react/react-in-jsx-scope": 0,
-    // React Hooks의 의존성 배열을 정확하게 명시하도록 경고
-    "react-hooks/exhaustive-deps": 1,
-    // TypeScript에서 'any' 타입의 명시적 사용을 허용
-    "@typescript-eslint/no-explicit-any": 0,
-  },
-
-  // ESLint 규칙에서 사용할 추가적인 설정. 여기서는 React 버전을 자동으로 감지하도록 설정한다.
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-
-  // ESLint가 파일을 무시하도록 하는 패턴들이다.
-  ignorePatterns: ["node_modules/", "build/", "dist/", "webpack.*.js"],
-};
+{
+  "extends": ["react-app", "plugin:prettier/recommended"],
+  "rules": {
+    "no-var": "warn", // var 금지
+    "no-multiple-empty-lines": "warn", // 여러 줄 공백 금지
+    "eqeqeq": "warn", // 일치 연산자 사용 필수
+    "dot-notation": "warn", // 가능하다면 dot notation 사용
+    "no-unused-vars": "warn", // 사용하지 않는 변수 금지
+    "react/destructuring-assignment": "warn", // state, prop 등에 구조분해 할당 적용
+    "react/no-direct-mutation-state": "warn", // state 직접 수정 금지
+    "react/no-unused-state": "warn", // 사용되지 않는 state
+    "react/jsx-curly-brace-presence": "warn" // jsx 내 불필요한 중괄호 금지
+  }
+}
