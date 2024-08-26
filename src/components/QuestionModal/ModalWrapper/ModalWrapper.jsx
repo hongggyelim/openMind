f78fc7848c498868504da065a34fe1d0d06b9ff7
hongggy <hongggy@gmail.com>
2024-08-26 16:21:07 +0900

    Design : 질문 모달창 ui 작업

--- src/components/QuestionModal/ModalWrapper/ModalWrapper.jsx
+++ src/components/QuestionModal/ModalWrapper/ModalWrapper.jsx
@@ -1,14 +1,22 @@
 import { QuestionForm } from '../QuestionForm/QuestionForm';
+import styles from './ModalWrapper.module.css';
 
 export function ModalWrapper() {
   return (
-    <div>
-      <div>
-        <div>bg</div>
-        질문을 작성하세요
+    <div className={styles.modalWrapper}>
+      <div className={styles.modalTitle}>
+        <div>
+          <span className={styles.motalIcon}></span>
+          질문을 작성하세요
+        </div>
+        <button className={styles.modalExitBtn}></button>
       </div>
+
       {/* 나중에 유저 프로필 이미지, 이름 prop으로 받아오기 */}
-      <div>To.아초는 고양이</div>
+      <div className={styles.modalUserName}>
+        <span className={styles.modalTo}>To.</span>
+        아초는 고양이
+      </div>
       <QuestionForm />
     </div>
   );
