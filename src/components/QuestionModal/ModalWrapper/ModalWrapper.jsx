1012acd1bd64295886fb2c5f90ef7f30c08149d1
hongggy <hongggy@gmail.com>
2024-08-26 18:12:44 +0900

    Feat : 답변 입력 시 제출 버튼 활성화

--- src/components/QuestionModal/ModalWrapper/ModalWrapper.jsx
+++ src/components/QuestionModal/ModalWrapper/ModalWrapper.jsx
@@ -6,30 +6,32 @@ export function ModalWrapper() {
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleClickExitBtn = () => {
-    setModalOpen(true);
+    setModalOpen(false);
   };
 
   return (
-    <div className={styles.modalWrapper}>
-      <div className={styles.modalHeader}>
-        <div className={styles.modalTitle}>
-          <span className={styles.modalIcon}></span>
-          질문을 작성하세요
+    <div className={styles.modalSpacer}>
+      <div className={styles.modalWrapper}>
+        <div className={styles.modalHeader}>
+          <div className={styles.modalTitle}>
+            <span className={styles.modalIcon}></span>
+            질문을 작성하세요
+          </div>
+          <button
+            type="button"
+            onClick={handleClickExitBtn}
+            className={styles.modalExitBtn}
+          ></button>
         </div>
-        <button
-          type="button"
-          onClick={handleClickExitBtn}
-          className={styles.modalExitBtn}
-        ></button>
-      </div>
 
-      {/* 나중에 유저 프로필 이미지, 이름 prop으로 받아오기 */}
-      <div className={styles.modalToWhon}>
-        <span className={styles.modalTo}>To.</span>
-        <span className={styles.modalUserProfile}></span>
-        아초는 고양이
+        {/* 나중에 유저 프로필 이미지, 이름 prop으로 받아오기 */}
+        <div className={styles.modalToWhon}>
+          <span className={styles.modalTo}>To.</span>
+          <span className={styles.modalUserProfile}></span>
+          아초는 고양이
+        </div>
+        <QuestionForm />
       </div>
-      <QuestionForm />
     </div>
   );
 }
--- src/components/QuestionModal/QuestionForm/QuestionForm.jsx
+++ src/components/QuestionModal/QuestionForm/QuestionForm.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import styles from './QuestionForm.module.css';
 export function QuestionForm() {
+  const [content, setContent] = useState('');
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  const handleChangeContent = e => {
+    const nextContent = e.target.value;
+    setContent(() => nextContent);
+
+    if (nextContent !== '') {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
+    }
+  };
+
   return (
     <form className={styles.questionForm}>
       <textarea
         className={styles.questionTextarea}
         placeholder="질문을 입력해주세요"
+        onChange={handleChangeContent}
+        value={content}
       ></textarea>
-      <button type="submit" className={styles.submitBtn}>
+      <button
+        disabled={isEmpty}
+        type="submit"
+        className={isEmpty ? styles.disabledBtn : styles.activeBtn}
+      >
         질문 보내기
       </button>
     </form>
