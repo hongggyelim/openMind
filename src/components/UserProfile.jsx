22346c923b63996612a41720e0fb5d4208ce4679
jjeankim <miyukikj35@gmail.com>
2024-08-27 10:31:31 +0900

    Rename: Demo 디렉토리 삭제

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 import styles from './UserProfile.module.css';
-import arrow from '../../assets/icon/ic-arrow-left.svg';
+import profile from '../../assets/images/user-profile.png';
 
 function UserProfile() {
   return (
     <div className={styles.profile}>
-      <img src={arrow} alt="화살표" className={styles['user-img']}></img>
+      <img src={profile} alt="화살표" className={styles['user-img']}></img>
       <span className={styles['user-nickname']}>아초는 고양이</span>
     </div>
   );
